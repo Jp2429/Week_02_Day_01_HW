0f8e1678cbb4c9eec8985a72c220c56718416703
Samuel Williams <samuel.williams.ed@gmail.com>
2022-11-28 15:53:16 +0000

    Student Class

--- single_class_lab/single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab/single_class_lab_start_code/student_class/src/student.py
@@ -1,1 +1,12 @@
+class Student():
+    def __init__(self, name, cohort):
+        self.name = name
+        self.cohort = cohort
+    def talk(self):
+        return "I can talk!"
+    def say_favourite_language(self, fav_language):
+        if fav_language == "Python":
+            return "I love Python"
+        else:
+            return "Wrong answer"
 
